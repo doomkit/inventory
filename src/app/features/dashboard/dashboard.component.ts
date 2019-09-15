6192369ea5d38cb180c5ef02aa78deb760f8625d
Nikita Shkarupa <n.shkarupa@yahoo.com>
2019-09-15 12:18:57 +0200

    create dashboard module

---
+++ src/app/features/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  template: `
+    <p>
+      dashboard Works!
+    </p>
+  `,
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
