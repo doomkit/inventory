a2e8cc9293f071a4a7a7fbe763c2537f11f7b847
Nikita Shkarupa <n.shkarupa@yahoo.com>
2019-09-15 23:13:15 +0200

    logout logic

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthGuard } from '@app/core/auth/auth.guard';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,11 +8,24 @@ import { Component, OnInit } from '@angular/core';
     <p>
       dashboard Works!
     </p>
+    <button (click)="logOut()">Log out</button>
   `,
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private authService: AuthGuard,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
+
+  logOut() {
+    sessionStorage.removeItem('username');
+    this.authService.canActivate(
+      this.route.snapshot,
+      this.router.routerState.snapshot
+    );
+  }
 }
