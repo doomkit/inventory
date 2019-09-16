1cae8b1f855d4968996a98d724e33c8f63186600
Nikita Shkarupa <n.shkarupa@yahoo.com>
2019-09-16 20:19:20 +0200

    remove: unused import from NotFoundComponent

--- src/app/page-not-found.component.ts
+++ src/app/page-not-found.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-page-not-found',
