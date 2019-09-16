5f73150883bb0526b1920582c22a29ccb64f8416
Nikita Shkarupa <n.shkarupa@yahoo.com>
2019-09-16 20:18:30 +0200

    add NotFoundPage layout + functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from '@shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/page-not-found.component.ts
+++ src/app/page-not-found.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-page-not-found',
   template: `
-    <p>
-      page-not-found Works!
-    </p>
+    <div class="page">
+      <span class="huge-text">404</span>
+      <div class="not-found-image"></div>
+      <h1>Sorry,<br />page not found</h1>
+      <app-button [text]="'Back'" (trigger)="redirect()"></app-button>
+    </div>
   `,
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent implements OnInit {
-  constructor() {}
+export class PageNotFoundComponent {
+  constructor(private router: Router) {}
 
-  ngOnInit() {}
+  redirect() {
+    this.router.navigate(['/dashboard']);
+  }
 }
