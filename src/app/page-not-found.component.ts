6cd17ced1e383efdda5794812a165dca58a3ad5e
Nikita Shkarupa <n.shkarupa@yahoo.com>
2019-09-22 17:55:22 +0200

    add vector author link

--- src/app/page-not-found.component.ts
+++ src/app/page-not-found.component.ts
@@ -6,7 +6,15 @@ import { Router } from '@angular/router';
   template: `
     <div class="page">
       <span class="huge-text">404</span>
-      <div class="not-found-image"></div>
+      <div class="not-found-image">
+        <a
+          href="https://www.freepik.com/free-photos-vectors/business"
+          target="_blank"
+          rel="noopener"
+        >
+          <span class="secondary-text">vector created by freepik</span>
+        </a>
+      </div>
       <h1>Sorry,<br />page not found</h1>
       <app-button [text]="'Back'" (trigger)="redirect()"></app-button>
     </div>
