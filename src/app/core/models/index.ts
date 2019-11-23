494eacd65cd6e55446595a7c0312364a7545c9ce
Nick <n.shkarupa@yahoo.com>
2019-11-23 16:52:34 +0100

    move Stock to interfaces

--- src/app/core/models/index.ts
+++ src/app/core/models/index.ts
@@ -1,9 +1,9 @@
-export * from './enums/user-role.enum';
-
 // ENUMS
-export * from './enums/stock-item-category.enum';
+export * from "./enums/stock-item-category.enum";
+export * from "./enums/user-role.enum";
 
 // INTERFACES
-export * from './interfaces/stock-item';
-export * from './interfaces/user';
-export * from './interfaces/order';
+export * from "./interfaces/stock";
+export * from "./interfaces/stock-item";
+export * from "./interfaces/user";
+export * from "./interfaces/order";
---
+++ src/app/core/models/interfaces/stock.ts
@@ -0,0 +1,6 @@
+export interface Stock {
+  id: number;
+  details: string;
+  square: string;
+  photo_url: string;
+}
