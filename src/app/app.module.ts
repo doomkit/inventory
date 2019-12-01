9fc4935b96205dea0095f88ec04bc29a92af0b26
Nikita Shkarupa <n.shkarupa@yahoo.com>
2019-12-01 13:06:56 +0100

    enable redux devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
-import { CoreModule } from './core/core.module';
-import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from '@shared/shared.module';
+import { CoreModule } from "./core/core.module";
+import { AppRoutingModule } from "./app-routing.module";
+import { SharedModule } from "@shared/shared.module";
 
-import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './page-not-found.component';
+import { AppComponent } from "./app.component";
+import { PageNotFoundComponent } from "./page-not-found.component";
+
+import { environment } from "@env/environment";
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent],
@@ -29,6 +32,11 @@ import { PageNotFoundComponent } from './page-not-found.component';
         }
       }
     ),
+    // Instrumentation must be imported after importing StoreModule
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production // Restrict extension to log-only mode
+    }),
     EffectsModule.forRoot([])
   ],
   providers: [],
