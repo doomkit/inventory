c913f379801ef19b7689bac1e1b504729040c8ef
Nikita Shkarupa <n.shkarupa@yahoo.com>
2019-12-01 23:41:36 +0100

    refactor: StockItems effects

--- src/app/core/store/effects/stock-items.effects.ts
+++ src/app/core/store/effects/stock-items.effects.ts
@@ -1,24 +1,17 @@
 import { Injectable } from '@angular/core';
-
 import { Actions, createEffect, ofType, Effect } from '@ngrx/effects';
 import { of } from 'rxjs';
-import { map, switchMap, catchError } from 'rxjs/operators';
+import { map, catchError, mergeMap } from 'rxjs/operators';
 import { StockItemsService } from '@app/core/services';
-
-import * as stockItemActions from '../actions/stock-items.action';
+import * as stockItemActions from '@core/store/actions/stock-items.action';
 
 @Injectable()
 export class StockItemsEffects {
-  constructor(
-    private actions$: Actions,
-    private stockItemsService: StockItemsService
-  ) {}
-
   @Effect()
   loadStockItems$ = createEffect(() =>
     this.actions$.pipe(
       ofType(stockItemActions.LOAD_ITEMS),
-      switchMap(() =>
+      mergeMap(() =>
         this.stockItemsService.getStockItems().pipe(
           map(items => new stockItemActions.LoadItemsSuccess(items)),
           catchError(error => of(new stockItemActions.LoadItemsFail(error)))
@@ -26,4 +19,9 @@ export class StockItemsEffects {
       )
     )
   );
+
+  constructor(
+    private actions$: Actions,
+    private stockItemsService: StockItemsService
+  ) {}
 }
