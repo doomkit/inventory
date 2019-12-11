15884ee185da0b8e87b85042dc31da35e4c72268
Nick <n.shkarupa@yahoo.com>
2019-12-11 20:54:13 +0100

    create: search pipe

--- src/app/shared/pipes/index.ts
+++ src/app/shared/pipes/index.ts
@@ -1,1 +1,1 @@
-// export * from ''
+export * from './search.pipe';
---
+++ src/app/shared/pipes/search.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+/*
+ * Filter array by selected parameter
+ * Usage:
+ *   data | search:by:val
+ * Example:
+ *   {{ [{name: 'test1'}, {name: 'test2}] | search:name:'test1' }}
+ *   returns [{name: 'test1'}]
+ */
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+  transform(data: any[], by?: string, val?: string): any {
+    if (!by || !val) {
+      return data;
+    }
+    return data.filter(el => el[by] === val);
+  }
+}
