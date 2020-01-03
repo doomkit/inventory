426e3b18d09b8670bee6be2b4d98bc7a56ddc29f
Nick <n.shkarupa@yahoo.com>
2020-01-03 16:23:46 +0100

    add: item-info pipe

---
+++ src/app/shared/pipes/item-info.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ItemInfoPipe } from './item-info.pipe';
+
+describe('Pipe: ItemInfoe', () => {
+  it('create an instance', () => {
+    let pipe = new ItemInfoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
